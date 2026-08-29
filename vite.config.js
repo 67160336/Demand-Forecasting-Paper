c7c3247de6e6ecacf53ffaabf8cc9c34c4d81efd
67160336 <67160336@go.buu.ac.th>
2026-08-29 07:29:44 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  // จุดสำคัญคือบรรทัดด้านล่างนี้ ต้องตรงกับชื่อ Repository บน GitHub
+  base: '/Demand-Forecasting-Paper/', 
+})
